c0cdf91f1b091bd2c35793769d3723bf3988cbee
Avinash <avinashkola2001@gmail.com>
2025-05-25 20:48:47 -0400

    Added requirements for deployment

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = []
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
